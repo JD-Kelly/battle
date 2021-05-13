c923914843f2bce28290f3b9e8c451eade818ede
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-13 15:43:04 +0100

    removed player global variables, added game global variable

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,12 @@
 class Game
+  attr_reader :player_1, :player_2
+
   def attack(player)
     player.damage
   end
 
   def initialize(player_1, player_2)
-
+    @player_1 = player_1
+    @player_2 = player_2
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,4 +17,16 @@ describe Game do
       expect(Game).to respond_to(:new).with(2).arguments
     end
   end
+
+  describe '#player_1' do
+    it 'returns player 1' do
+      expect(game.player_1).to eq(james)
+    end
+  end
+
+  describe '#player_2' do
+    it 'returns player 2' do
+      expect(game.player_2).to eq(kim)
+    end
+  end
 end
