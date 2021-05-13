629312b0961cd79e3df888d00644c5d256085e7e
“Jonny <jonny.kelly192@gmail.com>
2021-05-13 10:02:10 +0100

    refactors POST request to POST-redirect loop

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'view hit points' do 
+  scenario 'view player 2 hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'James'
+    fill_in :player_2_name, with: 'Kim'
+    click_button 'Submit'
+    expect(page).to have_content 'Kim: 60HP'
+  end
+end
