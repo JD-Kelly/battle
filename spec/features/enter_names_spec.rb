a84ef32558d2a47607e8d27102aac1d55988e041
“Jonny <jonny.kelly192@gmail.com>
2021-05-13 12:13:18 +0100

    created helper for sign in and play

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,8 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-  visit('/')
-  fill_in :player_1_name, with: 'James'
-  fill_in :player_2_name, with: 'Kim'
-  click_button 'Submit'
-
+  sign_in_and_play
   save_and_open_page
-  
+
   expect(page).to have_content 'James vs. Kim'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'view hit points' do 
   scenario 'view player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'James'
-    fill_in :player_2_name, with: 'Kim'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Kim: 60HP'
   end
 end
